eb7eb94876c0eb08f8af0c5b711213b56ccecb93
Prik <384128657@qq.com>
2019-10-28 22:14:59 +0800

    init

---
+++ prik/src/main/java/com/prik/test/ArrayListTest.java
@@ -0,0 +1,16 @@
+package com.prik.test;
+
+import java.util.ArrayList;
+
+/**
+ * @author prik
+ * @date 2019/10/28
+ */
+public class ArrayListTest {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("test");
+    }
+}
---
+++ prik/src/main/java/com/prik/test/HashMapTest.java
@@ -0,0 +1,16 @@
+package com.prik.test;
+
+import java.util.HashMap;
+
+/**
+ * @author prik
+ * @date 2019/10/28
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("test", null);
+
+    }
+}
